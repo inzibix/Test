e6bc58a9b237db87539febcd99a8388f7f1268d8
Timofey <yurevichtimofij@gmail.com>
2024-07-09 14:55:06 +0300

    калькулятор

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-
-for a in range (1, 10+1):
- print(a)
+first = int(input("первое число"))
+deystvie = (input("выберете действие +,-,/,*"))
+second = int(input("второе число"))
+def calculator(first,deystvie,second):
+ if deystvie == '+':
+  return first + second
+ elif deystvie == '-':
+  return first - second
+ elif deystvie == '/':
+  return first / second
+ elif deystvie == '*':
+  return first * second
+ else:
+  print("неподходяший символ")
+print (calculator(first,deystvie,second))
